6de3633f662433b808422cf6455a4679c5439579
Michał Świątek <michal.swiatek@netivo.pl>
2024-12-10 17:13:54 +0100

    Template changes

--- templates/class_main.php
+++ templates/class_main.php
@@ -9,6 +9,10 @@ namespace ${NAMESPACE};
 
 use \Netivo\Core\Theme as CoreTheme;
 
+if ( ! defined( 'ABSPATH' ) ) {
+	header( 'HTTP/1.0 404 Forbidden' );
+	exit;
+}
 class Main extends CoreTheme
 {
     protected function init()
